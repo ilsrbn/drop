244072117fa594584933a10316e740a76051323f
ilsrbn <serbini271@gmail.com>
2026-07-24 17:26:04 +0300

    feat: register Drop runtime aliases

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { addVitePlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { createDropSfcTransformPlugin } from './build/drop-vite-plugin'
 
 export interface ModuleOptions {
   enabled?: boolean
@@ -12,6 +13,14 @@ export default defineNuxtModule<ModuleOptions>({
   defaults: {
     enabled: true,
   },
-  setup() {
+  setup(_options, nuxt) {
+    const resolver = createResolver(import.meta.url)
+
+    nuxt.options.alias['#drop/context'] = resolver.resolve('./runtime/context')
+    nuxt.options.alias['#drop/state'] = resolver.resolve('./runtime/store')
+    nuxt.options.alias['#drop/reactivity'] = resolver.resolve('./runtime/reactivity')
+    nuxt.options.alias['#drop/runtime'] = resolver.resolve('./runtime/client')
+
+    addVitePlugin(createDropSfcTransformPlugin())
   },
 })
